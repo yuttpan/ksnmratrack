3f061478f36768b339df291593e43558c14cbf86
ยุทธพงษ์ อินทรวรรณ <yuttpanin@gmail.com>
2017-08-21 16:16:04 +0700

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { LoginPageModule } from '../pages/login/login.module';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -17,7 +18,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
+    IonicModule.forRoot(MyApp),LoginPageModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
