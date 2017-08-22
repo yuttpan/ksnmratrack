458b3d30b028270ad53c7375935d5b9217e5f9c7
yuttpan <yuttpanin@gmail.com>
2017-08-22 07:18:02 +0700

    22/8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
-import { LoginPageModule } from '../pages/login/login.module';
+import { LoginPageModule } from "../pages/login/login.module";
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
 
 @NgModule({
   declarations: [
@@ -18,18 +18,21 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),LoginPageModule,
+    IonicModule.forRoot(MyApp),LoginPageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+
+    
+    
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { LoginPage } from "../login/login";
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController ) {
 
   }
-
+goTologin(){
+  this.navCtrl.push(LoginPage)
+}
 }
